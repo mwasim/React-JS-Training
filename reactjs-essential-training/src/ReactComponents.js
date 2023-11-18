1b3b3ac5812707028d25f7ea86b15a6ee266b228
mwasim <mwasimse@gmail.com>
2023-11-18 19:20:09 +0500

    ReactJS Essential Training: Used images and React Fragments as a better alternative to using div tag

--- reactjs-essential-training/src/ReactComponents.js
+++ reactjs-essential-training/src/ReactComponents.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const dishes = [
     "Black Beans Soup",
     "Chicken Soup",
@@ -24,6 +26,8 @@ function Main(props) {
     return (
         <section>
             <p>We serve the most {props.adjective} food.</p>
+            <img src="https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&w=200&h=750&dpr=1"
+                alt="A waiter presents two plates at a fancy restuarant." />
             <ul>
                 {
                     props.dishes.map((dishObj) => (
@@ -47,11 +51,13 @@ function Footer(props) {
 
 function ReactComponents() {
     return (
-        <div>
-            <Header name="Rebecca" />
+        //<div> -- Instead of using Divs we can use React.Fragment
+        <React.Fragment>
+            <Header name="Lubna" />
             <Main adjective="delecious" dishes={dishObjects} />
             <Footer year={new Date().getFullYear()} />
-        </div>
+        </React.Fragment>
+        //</div>
     );
 }
 
