c3da813a92102935d922208eb119dfe4886e53d2
mwasim <mwasimse@gmail.com>
2023-11-18 21:18:08 +0500

    ReactJS Essential Training: useReducer Hook is used to extract functionality into separate code.

--- reactjs-essential-training/src/App.js
+++ reactjs-essential-training/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState, useReducer } from 'react';
 
 
 function App() {
+  const [checked, setChecked] = useState(false);
+  const [isChecked, setIsChecked] = useReducer(isChecked => !isChecked, false);
   return (
     <div className="App">
-        <p>Some text here</p>
+      <p>
+        <input type='checkbox' value={checked} onChange={() => setChecked((checked) => !checked)} />
+        <label>{checked ? "Checked" : "Not Checked"}</label>
+      </p>
+      <p>
+        {/* We've used useReducer here to extract the functionality set in the above paragraph's checkbox */}
+        <input type='checkbox' value={isChecked} onChange={setIsChecked} />
+        <label>{isChecked ? "Checked" : "UnChecked"}</label>
+      </p>
     </div>
   );
 }
