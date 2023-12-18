271a33b19d33fa679c53616c931f49a14dc553a8
mwasim <mwasimse@gmail.com>
2023-12-18 16:08:33 +0500

    TicTacToe - Implemented feature to show list of past moves as buttons using the history of moves recorded earlier.

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -115,6 +115,25 @@ export default function Game() {
     setXIsNext(!xIsNext);
   }
 
+  function jumpTo(nextMove){
+    //TODO
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if(move > 0) {
+      description = `Go to move #${move}`;
+    }else{
+      description = `Go to game start`;
+    }
+
+    return (
+      <li key={move}>
+        <button onClick={()=> jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
   return (
     <>
       <div className="game">
@@ -126,7 +145,7 @@ export default function Game() {
           />
         </div>
         <div className="game-info">
-          <ol>{/*TODO*/}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     </>
