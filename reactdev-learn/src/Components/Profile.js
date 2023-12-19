45310ddcc38638600ef6d3ea1bf81ccd5272181e
mwasim <mwasimse@gmail.com>
2023-12-19 18:45:22 +0500

    ReactDev-Learn: 1). Passing JSX as children demo 2). index.css file updated to use new styles

--- reactdev-learn/src/Components/Profile.js
+++ reactdev-learn/src/Components/Profile.js
@@ -1,16 +1,22 @@
 import { GetImageUrl } from "./Utils";
 
-function Avatar({ person, size=100 }) {
+function Avatar({ person, size = 100 }) {
   //const imageUrl = `https://i.imgur.com/${person.imageId}.jpg`;
 
   return (
     <img
+      className="avatar"
       src={GetImageUrl({ person })}
       alt={person.name}
       width={size}
       height={size}
     />
   );
+}
+
+//NOTE: This functon/component (Card) received JSX as an argument
+function Card({ children }) {
+  return <div className="card">{children}</div>;
 }
 
 export function NamedPorfile() {
@@ -22,14 +28,16 @@ export function NamedPorfile() {
 export default function Profile() {
   return (
     <>
-      <Avatar
-        person={{ name: "Katsuko Saruhashi", imageId: "YfeOqp2" }}
-        size={100}
-      />
-      <Avatar
-        person={{ name: "Lin Lanying", imageId: "1bX5QH6" }}
-        size={80}
-      />
+      <Card>
+        <Avatar
+          person={{ name: "Katsuko Saruhashi", imageId: "YfeOqp2" }}
+          size={100}
+        />
+      </Card>
+      <Card>
+        This is some text
+      </Card>
+      <Avatar person={{ name: "Lin Lanying", imageId: "1bX5QH6" }} size={80} />
     </>
   );
 }
