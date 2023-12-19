774472dbf5b4ec176ec6d74dc4f13d9d7ed6c409
mwasim <mwasimse@gmail.com>
2023-12-19 17:24:09 +0500

    ReactDev-Learn: Used JavaScript in JSX with curly braces - a demo

--- reactdev-learn/src/Components/TodoList.js
+++ reactdev-learn/src/Components/TodoList.js
@@ -1,18 +1,30 @@
+const person = {
+  name: "Hedy Lamarr",
+  avatarUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+  todoItems: [
+    "Invent new traffic lights",
+    "Rehearse a movie scene",
+    "Improve the spectrum technology",
+  ],
+  theme: { backgroundColor: "lightyellow", color: "darkgreen" },
+};
+
 function TodoList() {
+  const name = person.name;
+  const avatarUrl = person.avatarUrl;
+  const todoItems = person.todoItems;
+
+  const listItems = todoItems.map((item, index) => {
+    return <li key={index}>{item}</li>;
+    //console.log(`${item}, ${index}`);
+  });
+
   return (
-    <>
-      <h1>Hedy Lamarr's Todos</h1>
-      <img
-        src="https://i.imgur.com/yXOvdOSs.jpg"
-        alt="Hedy Lamarr"
-        className="photo"
-      />
-      <ul>
-        <li>Invent new traffic lights</li>
-        <li>Rehearse a movie scene</li>
-        <li>Improve the spectrum technology</li>
-      </ul>
-    </>
+    <div style={person.theme}>
+      <h1>{name}'s Todos</h1>
+      <img src={avatarUrl} alt={name} className="photo" />
+      <ul>{listItems}</ul>
+    </div>
   );
 }
 
