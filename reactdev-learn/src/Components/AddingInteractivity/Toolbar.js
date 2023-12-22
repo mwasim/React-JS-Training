fe833bcb92b629baa4785331f0be5eae6a94966f
mwasim <mwasimse@gmail.com>
2023-12-22 18:22:11 +0500

    ReactDevLearn: Adding Interactivity - stopping event propagation demo

--- reactdev-learn/src/Components/AddingInteractivity/Toolbar.js
+++ reactdev-learn/src/Components/AddingInteractivity/Toolbar.js
@@ -1,11 +1,26 @@
 function Button({ onClick, children }) {
-  return <button onClick={onClick}>{children}</button>;
+  return (
+    <button
+      onClick={(e) => {
+        e.stopPropagation(); //stop event propagation
+        onClick();
+      }}
+    >
+      {children}
+    </button>
+  );
 }
 
 export default function Toolbar({ onPlayMovie, onUploadImage }) {
-  return <div style={{backgroundColor: "darkgreen", padding: "10px"}} onClick={()=>{alert("You clicked on the Toolbar!")}}>
-    <Button onClick={onPlayMovie}>Play Movie</Button>
-    {' '}
-    <Button onClick={onUploadImage}>Upload Image</Button>
-  </div>;
+  return (
+    <div
+      style={{ backgroundColor: "darkgreen", padding: "10px" }}
+      onClick={() => {
+        alert("You clicked on the Toolbar!");
+      }}
+    >
+      <Button onClick={onPlayMovie}>Play Movie</Button>{" "}
+      <Button onClick={onUploadImage}>Upload Image</Button>
+    </div>
+  );
 }
