9914303c61ad9379bae5670bdc1d2a7452caa0e1
mwasim <mwasimse@gmail.com>
2023-12-23 14:13:35 +0500

    ReactDevLearn: useState hook with multiple states in single component demo

--- reactdev-learn/src/Components/AddingInteractivity/SculptureGallery.js
+++ reactdev-learn/src/Components/AddingInteractivity/SculptureGallery.js
@@ -4,12 +4,17 @@ import { useState } from "react";
 export default function SculptureGallery() {
   //let index = 0;
   const [index, setIndex] = useState(0); //pass intial value to useState
+  const [showMore, setShowMore] = useState(false);
   console.log(`index = ` + index);
 
   function handleClick() {
     //index = index + 1;
     setIndex(index + 1);
     console.log(`index = ` + index);
+  }
+
+  function handleShowMore() {
+    setShowMore(!showMore);
   }
 
   let sculpture = sculptureList[index];
@@ -24,8 +29,13 @@ export default function SculptureGallery() {
       <h3>
         {index + 1} of {sculptureList.length}
       </h3>
-      <img src={sculpture.url} alt={sculpture.alt} />
-      <p>{sculpture.description}</p>
+      <div style={{ marginBottom: "10px" }}>
+        <img src={sculpture.url} alt={sculpture.alt} />
+      </div>
+      <button onClick={handleShowMore}>
+        {showMore ? "Hide" : "Show"} Details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
     </>
   );
 }
