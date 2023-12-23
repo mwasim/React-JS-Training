3ecd6e0b90bcebff8d3108c19211e358f5b698d5
mwasim <mwasimse@gmail.com>
2023-12-23 16:26:00 +0500

    ReactDevLearn: useState hook - challenge completed. Implemented Prev/Next buttons with validation checks

--- reactdev-learn/src/Components/AddingInteractivity/SculptureGallery.js
+++ reactdev-learn/src/Components/AddingInteractivity/SculptureGallery.js
@@ -3,14 +3,37 @@ import { useState } from "react";
 
 export default function SculptureGallery() {
   //let index = 0;
+
+  /*
+    It is a good idea to have multiple state variables if their state is unrelated, 
+    like index and showMore in this example. 
+    
+    But if you find that you often change two state variables together, 
+    it might be easier to combine them into one. 
+    For example, if you have a form with many fields, 
+    it’s more convenient to have a single state variable that holds an object than state variable per field.
+  */
   const [index, setIndex] = useState(0); //pass intial value to useState
   const [showMore, setShowMore] = useState(false);
   console.log(`index = ` + index);
 
-  function handleClick() {
-    //index = index + 1;
-    setIndex(index + 1);
-    console.log(`index = ` + index);
+  let hasNext = index < sculptureList.length - 1;
+  let hasPrev = index > 0;
+
+  console.log("hasNext: " + hasNext + ", Items count: " + sculptureList.length);
+
+  function handlePrevClick() {
+    if (hasPrev) {
+      setIndex(index - 1);
+    }
+    //console.log(`index = ` + index);
+  }
+
+  function handleNextClick() {
+    if (hasNext) {
+      setIndex(index + 1);
+    }
+    //console.log(`index = ` + index);
   }
 
   function handleShowMore() {
@@ -21,7 +44,13 @@ export default function SculptureGallery() {
 
   return (
     <>
-      <button onClick={handleClick}>Next</button>
+      <button disabled={!hasPrev} onClick={handlePrevClick}>
+        Prev
+      </button>
+      {' '}
+      <button disabled={!hasNext} onClick={handleNextClick}>
+        Next
+      </button>
       <h2>
         <i>{sculpture.name}</i>
         by {sculpture.artist}
