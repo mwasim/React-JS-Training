6e3b7ad74889534cf0e5186f68f5db1b000deb15
mwasim <mwasimse@gmail.com>
2023-12-30 13:46:20 +0500

    ReactDevLearn: Do not read or write ref.current during rendering as it may cause unexpected results. Use it when your component needs to store some value but it does not impact the rendering logic

--- reactdev-learn/src/Components/EscapeHatches/UseRefSimpleDemo.js
+++ reactdev-learn/src/Components/EscapeHatches/UseRefSimpleDemo.js
@@ -2,11 +2,11 @@ import { useRef } from "react";
 
 export default function UseRefBasedCounter() {
   let ref = useRef(0);
-
-  function handleClick() {
+  
+  function handleClick(e) {
     ref.current = ref.current + 1;
     console.log(`You clicked ${ref.current} times.`);
   }
 
-  return <button onClick={handleClick}>Counter {ref.current}</button>;
+  return <button onClick={handleClick}>Counter</button>;
 }
--- reactdev-learn/src/index.js
+++ reactdev-learn/src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import DescribingTheUI from './DescribingTheUI'; //Uncomment to render DescribingTheUI
-import AddingInteractivity from "./AddingInteractivity";
+// import AddingInteractivity from "./AddingInteractivity";
+import EscapeHatches from "./EscapeHatches";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <DescribingTheUI /> //Uncomment to render DescribingTheUI */}
-    <AddingInteractivity />
+    {/* <AddingInteractivity /> */}
+    <EscapeHatches />
   </React.StrictMode>
 );
