e23cfc104575a74fd06209a12bd4e095c10fcb52
Wasim <mwasimse@gmail.com>
2024-02-10 18:03:46 +0500

    ReactTypescriptSuperHero - Login component implemented and used in the Login page.

---
+++ react-typescript-superhero/src/Components/Login.tsx
@@ -0,0 +1,17 @@
+import Input from "./Input";
+
+const Login = () => {
+  return (
+    <div className="w-full md:w-[450px]">
+      <h1 className="text-white text-center font-bold text-4xl md:text-6xl mb-10">
+        Login
+      </h1>
+      <div className="bg-white w-full p-6 min-h-[150px] flex flex-col gap-3 rounded-xl drop-shadow-xl">
+        <Input name="email" type="email" />
+        <Input name="password" type="password" />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- react-typescript-superhero/src/Pages/LoginPage.tsx
+++ react-typescript-superhero/src/Pages/LoginPage.tsx
@@ -1,18 +1,9 @@
-import React from "react";
-import Input from "../Components/Input";
+import Login from "../Components/Login";
 
 const LoginPage = () => {
   return (
     <div className="bg-blue-600 h-[100vh] flex items-center justify-center p-10">
-      <div className="w-full md:w-[450px]">
-        <h1 className="text-white text-center font-bold text-4xl md:text-6xl mb-10">
-          Login
-        </h1>
-        <div className="bg-white w-full p-6 min-h-[150px] flex flex-col gap-3 rounded-xl drop-shadow-xl">
-          <Input name="email" type="email" />
-          <Input name="password" type="password" />
-        </div>
-      </div>
+      <Login />
     </div>
   );
 };
