2aa8cf48d302be27b476d161850296706ab9ebbb
Wasim <mwasimse@gmail.com>
2024-02-15 16:34:28 +0500

    ReactTypescriptSuperHero - Implemented useState hook to switch between Login and Register views.

--- react-typescript-superhero/src/Components/Login.tsx
+++ react-typescript-superhero/src/Components/Login.tsx
@@ -1,17 +1,42 @@
+import { useState } from "react";
 import Button from "./Button";
 import Input from "./Input";
 
 const Login = () => {
+  const [login, setLogin] = useState(true);
+
   return (
     <div className="w-full md:w-[450px]">
       <h1 className="text-white text-center font-bold text-4xl md:text-6xl mb-10">
-        Login
+        {login ? "Login" : "Register"}
       </h1>
       <div className="bg-white w-full p-6 min-h-[150px] flex flex-col gap-3 rounded-xl drop-shadow-xl">
         <Input name="email" type="email" />
         <Input name="password" type="password" />
-        <Button text="Login" loading />
-        <Button text="Register" secondary loading />
+        {!login && <Input name="confirm-password" type="password" />}
+        {login ? (
+          <>
+            <Button text="Login" />
+            <Button
+              text="Register"
+              onClick={() => {
+                setLogin(false);
+              }}
+              secondary
+            />
+          </>
+        ) : (
+          <>
+            <Button text="Register" />
+            <Button
+              text="Login"
+              onClick={() => {
+                setLogin(true);
+              }}
+              secondary
+            />
+          </>
+        )}
       </div>
     </div>
   );
