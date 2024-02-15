f1b687c5dd67549d035f4d7e9917ba1c308704b1
Wasim <mwasimse@gmail.com>
2024-02-15 15:20:56 +0500

    ReactTypescriptSuperHero - Background and background pattern added on the Login page.

--- react-typescript-superhero/src/Pages/LoginPage.tsx
+++ react-typescript-superhero/src/Pages/LoginPage.tsx
@@ -2,8 +2,10 @@ import Login from "../Components/Login";
 
 const LoginPage = () => {
   return (
-    <div className="bg-blue-600 h-[100vh] flex items-center justify-center p-10">
+    <div className="h-[100vh] flex items-center justify-center p-10">
       <Login />
+      <div className="h-full w-full bg-gradient-to-r from-myBlue to-myPink absolute top-0 -z-10 opacity-70" />
+      <div className="h-full w-full absolute bg-pattern -z-20 top-0"></div>
     </div>
   );
 };
