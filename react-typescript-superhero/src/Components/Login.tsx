c95cbbf9c1bab5bd820a07d6bba1ab3341951edd
Wasim <mwasimse@gmail.com>
2024-02-15 17:45:19 +0500

    ReactTypescriptSuperHero - Implemented state retention for Login/Register forms fields. And implemented initial event handlers to handle Login/Register click events.

--- react-typescript-superhero/src/Components/Login.tsx
+++ react-typescript-superhero/src/Components/Login.tsx
@@ -4,6 +4,19 @@ import Input from "./Input";
 
 const Login = () => {
   const [login, setLogin] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSignIn = () => {
+    let data = { email, password };
+    console.log(data);
+  };
+
+  const handleSignUp = () => {
+    let data = { email, password, confirmPassword };
+    console.log(data);
+  };
 
   return (
     <div className="w-full md:w-[450px]">
@@ -11,12 +24,35 @@ const Login = () => {
         {login ? "Login" : "Register"}
       </h1>
       <div className="bg-white w-full p-6 min-h-[150px] flex flex-col gap-3 rounded-xl drop-shadow-xl">
-        <Input name="email" type="email" />
-        <Input name="password" type="password" />
-        {!login && <Input name="confirm-password" type="password" />}
+        <Input
+          name="email"
+          type="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <Input
+          name="password"
+          type="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        {!login && (
+          <Input
+            name="confirm-password"
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => {
+              setConfirmPassword(e.target.value);
+            }}
+          />
+        )}
         {login ? (
           <>
-            <Button text="Login" />
+            <Button text="Login" onClick={handleSignIn} />
             <Button
               text="Register"
               onClick={() => {
@@ -27,7 +63,7 @@ const Login = () => {
           </>
         ) : (
           <>
-            <Button text="Register" />
+            <Button text="Register" onClick={handleSignUp} />
             <Button
               text="Login"
               onClick={() => {
