1f1d810439a0a54e030505511308c5045ba45c1c
Wasim <mwasimse@gmail.com>
2024-03-08 16:01:16 +0500

    ToDoList React JS Demo - React and Tailwind CSS setup.

---
+++ todo-list-react-tailwind/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-center h-screen bg-gradient-to-r from-purple-400 via-pink-200 to-pink-300 text-white">
+      <h1 className=" text-violet-900 mg-8 text-5xl pt-12 font-bold">
+        To do List
+      </h1>
+    </div>
+  );
+}
+
+export default App;
