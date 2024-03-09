296049548f32eb9bc35b76195c22202faadbd8a0
Wasim <mwasimse@gmail.com>
2024-03-09 15:39:59 +0500

    ToDoList React JS Demo - implemented Add Task to List functionality.

--- todo-list-react-tailwind/src/Components/ToDoMenu.jsx
+++ todo-list-react-tailwind/src/Components/ToDoMenu.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { GrFormAdd } from "react-icons/gr";
 
 const ToDoMenu = () => {
+  const [task, setTask] = useState(``);
+  const [list, setList] = useState([]);
+
+  const addTask = () => {
+    if (task) {
+      const toDo = {
+        id: list.length + 1,
+        title: task,
+        toggle: false,
+      };
+
+      setList(toDo, ...list);
+
+      //reset task
+      setTask(``);
+    }
+  };
+
+  console.log(list);
+
   return (
     <div className="mx-auto mt-8">
       <div className=" flex items-center justify-center mb-4">
@@ -9,8 +29,15 @@ const ToDoMenu = () => {
           type="text"
           className="w-[350px] border-2 border-blue-900 bg-gradient-to-r from-sky-900 via-sky-500 to-indigo-200 font-bold rounded-md px-3 py-2 mr-3 placeholder:text-white"
           placeholder="Enter Task..."
+          value={task}
+          onChange={(e) => {
+            setTask(e.target.value);
+          }}
         />
-        <button className="bg-blue-500 hover:bg-blue-600 font-bold py-4 px-4 rounded">
+        <button
+          className="bg-blue-500 hover:bg-blue-600 font-bold py-4 px-4 rounded"
+          onClick={addTask}
+        >
           <GrFormAdd />
         </button>
       </div>
