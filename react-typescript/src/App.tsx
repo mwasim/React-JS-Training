2fdb99ef05d4b9be52f2b582d333bae5d342c056
Wasim <mwasimse@gmail.com>
2024-03-12 20:00:08 +0500

    ReactUsingTypescript: Typescript basic data types demo

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,8 +1,13 @@
 import "./App.css";
 
 function App() {
-  let firstValue: string = "Many";
+  //let firstValue: string = "Many";
+  //let firstValue: number = 5;
+  //let firstValue: boolean = true;
 
+  //Array syntax
+  //let firstValue: number[] = [1, 2, 3, 4, 5];
+  let firstValue: Array<string> = ["one", "two", "three"];
   return (
     <div className="App">
       <header className="App-header">
