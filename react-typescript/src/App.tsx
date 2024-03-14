dffeea9c524af5bf0319fe39206d4d9f2df97008
Wasim <mwasimse@gmail.com>
2024-03-14 09:11:46 +0500

    ReactUsingTypescript: Stateless function component demo

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,14 +1,16 @@
 import "./App.css";
 import "./basic_data_types";
 // import BasicDataTypes from "./basic_data_types";
-import ComplexDataTypes from "./complex_data_types";
+//import ComplexDataTypes from "./complex_data_types";
+import Message from "./Message";
 
 function App() {
   return (
-    <>
-      {/* <BasicDataTypes /> */}
-      <ComplexDataTypes />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <Message message="This is a message!" />
+      </header>
+    </div>
   );
 }
 
---
+++ react-typescript/src/Message.tsx
@@ -0,0 +1,5 @@
+const Message = (prop: { message: string }): any => {
+  return <p>{prop.message}</p>;
+};
+
+export default Message;
