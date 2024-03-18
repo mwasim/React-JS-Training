97970a5c2e4dc57e8ff514e52bebfd71cfbe66f2
Wasim <mwasimse@gmail.com>
2024-03-18 11:31:09 +0500

    ReactUsingTypescript: class component syntax for react libraries demo.

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,17 +1,29 @@
+import { Component, ReactNode } from "react";
 import "./App.css";
 import "./basic_data_types";
 // import BasicDataTypes from "./basic_data_types";
 //import ComplexDataTypes from "./complex_data_types";
 import Message from "./Message";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Message message="This is a message!" />
-      </header>
-    </div>
-  );
+//class/stateful component syntax
+class App extends Component<any> {
+  componentDidMount(): void {
+    console.log(`Finally... hello!`);
+  }
+
+  // componentWillMount(): void {
+  //   console.log(`Almost there...`);
+  // }
+
+  render(): ReactNode {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Message message="This is a message!" />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
